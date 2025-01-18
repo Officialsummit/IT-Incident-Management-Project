d89d0f7170768bf944ac43303ce48adc811a86bf
Officialsummit <xettrisumit@gmail.com>
2025-01-18 23:05:26 +1300

    Implemented IT incident management complete Datawarehouse project for semester 2 in my Master's degree

---
+++ scripts/table creation query.sql
@@ -0,0 +1,139 @@
+create database incident_test
+
+use incident_test
+CREATE TABLE dim_status (
+    idStatus INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    active BIT NOT NULL,
+    status_description VARCHAR(50) NOT NULL,
+    date_Stamp DATETIME NULL,
+    data_source VARCHAR(100) NULL
+);
+
+CREATE TABLE dim_employee (
+    Employee_number  varchar(50) PRIMARY KEY NOT NULL,
+    date_Stamp DATETIME NULL,
+    data_source VARCHAR(100) NULL
+);
+
+CREATE TABLE dim_priority (
+    idPriority INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    impact VARCHAR(50) NOT NULL,
+    urgency VARCHAR(50) NOT NULL,
+    priority VARCHAR(50) NOT NULL,
+    date_Stamp DATETIME NULL,
+    data_source VARCHAR(100) NULL
+);
+
+CREATE TABLE dim_date (
+    idDate INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    date DATETIME NOT NULL,
+    year VARCHAR(50) NOT NULL,
+    month INT NOT NULL,
+    day INT NOT NULL,
+	date_Stamp DATETIME NULL,
+    data_source VARCHAR(100) NULL
+  
+);
+
+CREATE TABLE dim_category (
+    idCategory INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    subcategory VARCHAR(50) NOT NULL,
+    date_Stamp DATETIME NULL,
+    data_source VARCHAR(100) NULL
+);
+
+
+CREATE TABLE dim_Reported (
+    idIncident INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    incident_number VARCHAR(50) NOT NULL,
+	location VARCHAR(50) NOT NULL,
+    customer_number VARCHAR(50) NOT NULL,
+    contact_type VARCHAR(50) NOT NULL,
+	date_Stamp DATETIME NULL,
+    data_source VARCHAR(100) NULL
+);
+
+
+
+FactTable:
+CREATE TABLE fact_Incident_Management (
+    idIncidentMgmt INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    idIncident INT NOT NULL,
+    Responsible VARCHAR(50) NOT NULL,
+    idStatus INT NOT NULL,
+    idPriority INT NOT NULL,
+    idCategory INT NOT NULL,
+    Opened_by VARCHAR(50) NOT NULL,
+    Opened_at Int NOT NULL,
+    Created_by VARCHAR(50) NOT NULL,
+    Created_at Int NOT NULL,
+    Updated_by VARCHAR(50) NOT NULL,
+    Updated_at Int NOT NULL,
+    Resolved_by VARCHAR(50) NOT NULL,
+    Resolved_at Int NOT NULL,
+    Closed_at Int NOT NULL,
+    Close_code VARCHAR(50) NULL,
+    SLA TinyInt NULL,
+    Notify TinyInt NULL,
+    Knowledge TinyInt NULL,
+    Priority_confirmation TinyInt NULL,
+    Customer_symptom VARCHAR(255) NULL,
+    Reassignment_count INT NULL,
+    Reopen_count INT NULL,
+    Modify_count INT NULL,
+    Assigned_Group VARCHAR(50) NULL,
+    date_stamp DATETIME NULL,
+    Data_Source VARCHAR(50) NULL,
+    CONSTRAINT FK_dim_Reported FOREIGN KEY (idIncident) REFERENCES dim_Reported(idIncident),
+    CONSTRAINT FK_dim_Employee_Refponsible FOREIGN KEY (Responsible) REFERENCES dim_employee(Employee_number),
+    CONSTRAINT FK_dim_status FOREIGN KEY (idStatus) REFERENCES dim_status(idStatus),
+    CONSTRAINT FK_dim_priority FOREIGN KEY (idPriority) REFERENCES dim_priority(idPriority),
+    CONSTRAINT FK_dim_category FOREIGN KEY (idCategory) REFERENCES dim_category(idCategory),
+    CONSTRAINT FK_dim_Employee_Opened_by FOREIGN KEY (Opened_by) REFERENCES dim_employee(Employee_number),
+    CONSTRAINT FK_dim_date_Opened_at FOREIGN KEY (Opened_at) REFERENCES dim_date(idDate),
+    CONSTRAINT FK_dim_Employee_Created_by FOREIGN KEY (Created_by) REFERENCES dim_employee(Employee_number),
+    CONSTRAINT FK_dim_date_Created_at FOREIGN KEY (Created_at) REFERENCES dim_date(idDate),
+    CONSTRAINT FK_dim_Employee_Updated_by FOREIGN KEY (Updated_by) REFERENCES dim_employee(Employee_number),
+    CONSTRAINT FK_dim_date_Updated_at FOREIGN KEY (Updated_at) REFERENCES dim_date(idDate),
+    CONSTRAINT FK_dim_Employee_Resolved_by FOREIGN KEY (Resolved_by) REFERENCES dim_employee(Employee_number),
+    CONSTRAINT FK_dim_date_Resolved_at FOREIGN KEY (Resolved_at) REFERENCES dim_date(idDate),
+    CONSTRAINT FK_dim_date_Close_at FOREIGN KEY (Closed_at) REFERENCES dim_date(idDate)
+);
+
+
+
+--use incident_test
+use servicenow_incidents_db
+--Delete From staging_table
+--Delete  from fact_Incident_Management
+--Delete  from dim_category
+--Delete  from dim_status
+--Delete  from dim_date
+--Delete  from dim_employee
+--Delete  from dim_Reported
+--Delete  from dim_priority
+
+
+use servicenow_incidents_db
+
+select * from staging_table
+
+SELECT  *  FROM incident_test.[dbo].[dim_employee]
+	   
+SELECT  *  FROM dim_priority
+	   
+SELECT  *  FROM dim_status
+	   
+SELECT  *    FROM dim_category
+	   
+SELECT  *    FROM dim_reported 
+	   
+SELECT  *    FROM dim_date where date = null
+
+SELECT  count(*)    FROM fact_incident_management where Updated_by = 'UpdatedBy908'
+
+select a.idDate, a.date from dim_date a join fact_Incident_Management b 
+on a.idDate = b.Created_at 
+where b.idIncident = '24919'
+
